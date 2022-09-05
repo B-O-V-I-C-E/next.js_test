e0199210aaada68e55c7a064fce2c4ac505a1c1f
Cody Grace <codyrichardgrace@gmail.com>
2022-09-05 02:38:30 -0400

    Spacing Update

--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,15 @@ export default function Home() {
     <div className="section">
         <div className="row">
           <div className="col">
-            <h2>This is the first section</h2>
-            <p> Curabitur quis libero ullamcorper, vulputate ipsum non, malesuada ipsum. Nam facilisis tellus ante, in imperdiet nulla viverra at. 
-              Donec tincidunt purus nisi, in fermentum nibh efficitur sit amet. Sed aliquet, est in tincidunt consequat, quam lorem varius eros, eu 
-              luctus purus tortor eu arcu. Fusce hendrerit faucibus gravida. Duis massa dolor, elementum sit amet cursus et, finibus ac tellus. Sed 
-              vitae finibus ipsum, ut vehicula risus. Nunc dignissim justo nunc, ut fermentum nulla maximus a. Pellentesque pellentesque sollicitudin sem 
-              quis tristique. Nunc malesuada metus lobortis diam efficitur, quis dictum dui fringilla. Etiam massa enim, mollis sit amet hendrerit vitae, 
-              vestibulum vel lorem. Mauris pharetra erat tellus, id dictum massa dictum id. Sed sit amet nisl dictum, ultricies elit et, facilisis dui. 
-              Vivamus vel nunc semper quam molestie finibus eget at metus. </p>
+
+            <div className="text_block left">
+              <h2>This is the first section</h2>
+              <p> Curabitur quis libero ullamcorper, vulputate ipsum non, malesuada ipsum. Nam facilisis tellus ante, in imperdiet nulla viverra at. 
+                Donec tincidunt purus nisi, in fermentum nibh efficitur sit amet. Sed aliquet, est in tincidunt consequat, quam lorem varius eros, eu 
+                luctus purus tortor eu arcu. Fusce hendrerit faucibus gravida. Duis massa dolor, elementum sit amet cursus et, finibus ac tellus. Sed 
+                vitae finibus ipsum, ut vehicula risus.</p>
+            </div>
+
           </div>
           
           <div className="col">
