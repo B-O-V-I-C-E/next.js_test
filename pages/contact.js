433388b081abf33dcb7d4bd269781a3ff92ce20b
Cody Grace <codyrichardgrace@gmail.com>
2022-09-05 22:47:30 -0400

    Starting Animations

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ import CTA from "../comps/CTA";
 import subpage from "../styles/Subpage.module.css";
 
 const Contact = () => {
-    return ( 
+    return (
         <>
             <div className={[subpage.subPageHeader, "section"].join(" ")}>
                 <div className="row">
@@ -13,9 +13,29 @@ const Contact = () => {
                 </div>
             </div>
 
-            <CTA />
+            <div  className={[subpage.standout, "section"].join(" ")}>
+                <div className="row">
+                    <div className="col">
+                        <form action="/send-data-here" method="post">
+                            <label for="roll">Roll Number</label>
+                            <input
+                                type="text"
+                                id="roll"
+                                name="roll"
+                                required
+                                minlength="10"
+                                maxlength="20"
+                            />
+                            <label for="name">Name:</label>
+                            <input type="text" id="name" name="name" required />
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </>
-     );
+    );
 }
- 
+
 export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,47 @@ import Image from "next/image";
 import Link from "next/link";
 import CTA from "../comps/CTA";
 import styles from "../styles/Home.module.css";
+import { motion, AnimatePresence } from "framer-motion"
+
+const container = {
+  hidden: { 
+    opacity: 0,
+    scale: .98,
+    transition: {
+      staggerChildren: 0.25,
+    }
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      staggerChildren: 0.25,
+    }
+  }
+}
+
+const item = {
+  hidden: { 
+    opacity: 0,
+    scale: .98,
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+  },
+}
 
 export default function Home() {
   return (
     <>
-      <div className={[styles.hero, "section"].join(" ")}>
-        <div className="row">
+      <AnimatePresence><motion.div className={[styles.hero, "section"].join(" ")} 
+      variants={container} initial="hidden" whileInView={"show"} exit={{ scaleY: 0 }}>
+        <motion.div className="row" variants={item}>
           <div className="col">
             <h1>Welcome Home</h1>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div></AnimatePresence>
 
       <div className="section">
 
@@ -20,13 +50,13 @@ export default function Home() {
 
           <div className="col">
 
-            <div className="text_block left">
-              <h2>This is the first section</h2>
-              <p> Curabitur quis libero ullamcorper, vulputate ipsum non, malesuada ipsum. Nam facilisis tellus ante, in imperdiet nulla viverra at.
+            <motion.div className="text_block left" variants={container} initial={"hidden"} whileInView={"show"} viewport={{ amount: "some", margin: "-150px", once: true}}>
+              <motion.h2 variants={item}>This is the first section</motion.h2>
+              <motion.p variants={item}> Curabitur quis libero ullamcorper, vulputate ipsum non, malesuada ipsum. Nam facilisis tellus ante, in imperdiet nulla viverra at.
                 Donec tincidunt purus nisi, in fermentum nibh efficitur sit amet. Sed aliquet, est in tincidunt consequat, quam lorem varius eros, eu
-                luctus purus tortor eu arcu.</p>
-              <div className="button"><Link href={"/contact"}><a>Get Started</a></Link></div>
-            </div>
+                luctus purus tortor eu arcu.</motion.p>
+              <motion.div variants={item} className="button"><Link href={"/contact"}><a>Get Started</a></Link></motion.div>
+            </motion.div>
           </div>
 
           <div className="col">
@@ -42,12 +72,13 @@ export default function Home() {
           </div>
 
           <div className="col">
-            <div className="text_block right">
-              <h2>This is the second section</h2>
-              <p> Est in tincidunt consequat, quam lorem varius eros, eu luctus purus tortor eu arcu. Fusce hendrerit faucibus gravida. Duis massa dolor, 
-                elementum sit amet cursus et, finibus ac tellus. Sed vitae finibus ipsum, ut vehicula risus.</p>
-                <div className="button"><Link href={"/contact"}><a>Get Started</a></Link></div>
-            </div>
+          <motion.div className="text_block right" variants={container} initial={"hidden"} whileInView={"show"} viewport={{ amount: "some", margin: "-150px", once: true }}>
+              <motion.h2 variants={item}>This is the first section</motion.h2>
+              <motion.p variants={item}> Curabitur quis libero ullamcorper, vulputate ipsum non, malesuada ipsum. Nam facilisis tellus ante, in imperdiet nulla viverra at.
+                Donec tincidunt purus nisi, in fermentum nibh efficitur sit amet. Sed aliquet, est in tincidunt consequat, quam lorem varius eros, eu
+                luctus purus tortor eu arcu.</motion.p>
+              <motion.div variants={item} className="button"><Link href={"/contact"}><a>Get Started</a></Link></motion.div>
+            </motion.div>
           </div>
 
         </div>
